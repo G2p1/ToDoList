8e742d0a7d13c30d1b1f0ea7fd7fa966396e983a
G2p1 <andrii.tiuitunnyk@gmail.com>
2024-03-19 22:30:20 +0200

    feat:raplace task to tasklist
    Signed-off-by: G2p1 <andrii.tiuitunnyk@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
-import Task from './components/Task'
+import TaskList from './components/TaskList'
+
 function App() {
 
   return (
     <>
-    <Task></Task>
+    <TaskList></TaskList>
     </>
   )
 }
