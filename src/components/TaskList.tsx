a1d632c3bc33fce004edf1ade98476632ad8475f
G2p1 <andrii.tiuitunnyk@gmail.com>
2024-03-21 10:35:01 +0200

    feat:add createfunction and delete for TaskLst
    Signed-off-by: G2p1 <andrii.tiuitunnyk@gmail.com>

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,29 +1,65 @@
-import {useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import TaskType from "../types/TaskType";
 import Task from "./Task";
+import './css/Button.css'
+import './css/TaskList.css'
 import { uid } from "uid";
 
-const TaskList = () =>{
-    const [tasks, setTasks] = useState(Array<TaskType>)
+const TaskList = () => {
+  const [tasks, setTasks] = useState(Array<TaskType>);
 
-    useEffect(()=>{
-        let TaskArray:Array<TaskType>=[];
-        const defaultTask = {
-            id:uid(),
-            head: 'Task name',
-            body:'Task Body'
-        }
-        TaskArray.push(defaultTask);
-        setTasks(TaskArray);
-    },[]);
+  useEffect(() => {
+    const defaultTask = {
+      id: uid(),
+      head: "Task name",
+      body: "Task Body",
+    };
+    setTasks([...tasks, defaultTask]);
+  }, []);
 
-    return (
-    <>
-    {tasks.map((task:TaskType)=>{
-        return <div key={task.id}><Task></Task></div>
-    })}
-    </>);
-}
+  function createTask() {
+    const addTask = {
+      id: uid(),
+      head: "Task name",
+      body: "Task Body",
+    };
+    setTasks([...tasks, addTask]);
+
+  }
+
+  function remooveTask(id: string) {
+    setTasks(tasks.filter((item) => item.id !== id));
+  }
+
+    function changeContent(id:string, head:string, body:string){
+        let index = tasks.findIndex(item=> item.id == id);
+        tasks[index].body=body;
+        tasks[index].head=head;
+    }
+
+  return (
+    <Fragment>
+        <div className="tasklist">
+      {tasks.map((task: TaskType) => {
+        return (
+          <div key={task.id}>
+            <Task id={task.id} head={task.head} body={task.body} changeContent={changeContent}>
+              <button
+                className="rm-button"
+                onClick={() => remooveTask(task.id)}
+              >
+                -
+              </button>
+            </Task>
+          </div>
+        );
+      })}
+      <button className="cr-button" onClick={createTask}>
+        +
+      </button>
+      </div>
+    </Fragment>
+  );
+};
 
 export default TaskList;
-
